6340d9fda07e4fe4744010a7bb09aac4bfab45ff
nickmoreton <nickmoreton@me.com>
2024-10-28 22:02:56 +0000

    Added HeadingBlock and BlockQuoteBlock

--- home/blocks.py
+++ home/blocks.py
@@ -1,5 +1,36 @@
 from wagtail import blocks
 
 
+class HeadingBlock(blocks.StructBlock):
+    level = blocks.ChoiceBlock(
+        required=True,
+        choices=[
+            ("h1", "H1"),
+            ("h2", "H2"),
+            ("h3", "H3"),
+            ("h4", "H4"),
+            ("h5", "H5"),
+            ("h6", "H6"),
+        ],
+        help_text="Level of heading",
+    )
+    text = blocks.CharBlock(required=True, help_text="Text to display")
+
+    class Meta:
+        icon = "title"
+        template = "home/blocks/heading_block.html"
+
+
+class BlockQuoteBlock(blocks.StructBlock):
+    quote = blocks.TextBlock(required=True, help_text="Quote to display")
+    attribution = blocks.CharBlock(required=False, help_text="Attribution for quote")
+
+    class Meta:
+        icon = "openquote"
+        template = "home/blocks/blockquote_block.html"
+
+
 class StreamBlocks(blocks.StreamBlock):
     paragraph = blocks.RichTextBlock()
+    heading = HeadingBlock()
+    blockquote = BlockQuoteBlock()
--- wp_connector/streamfieldable.py
+++ wp_connector/streamfieldable.py
@@ -2,6 +2,37 @@ import json
 from dataclasses import dataclass, field
 
 from bs4 import BeautifulSoup as bs
+from django.utils.module_loading import import_string
+
+# from home.blocks import StreamBlocks, HeadingBlock, ParagraphBlock
+
+
+def build_paragraph_block(tag):
+    block = {
+        "type": "paragraph",
+        "value": f"{tag}",
+    }
+    return block
+
+
+def build_heading_block(tag):
+    block_dict = {
+        "type": "heading",
+        "value": {"text": tag.text, "level": tag.name},
+    }
+    return block_dict
+
+
+def build_blockquote_block(tag):
+    cite = tag.attrs.get("cite")
+    block = {
+        "type": "blockquote",
+        "value": {
+            "quote": tag.text,
+            "attribution": cite,
+        },
+    }
+    return block
 
 
 @dataclass
@@ -12,6 +43,16 @@ class StreamFieldable:
 
     content: str
     streamdata: json = field(init=False)
+    tags_to_blocks = {
+        "p": "build_paragraph_block",
+        "h1": "build_heading_block",
+        "h2": "build_heading_block",
+        "h3": "build_heading_block",
+        "h4": "build_heading_block",
+        "h5": "build_heading_block",
+        "h6": "build_heading_block",
+        "blockquote": "build_blockquote_block",
+    }
 
     def __post_init__(self):
         self.streamdata = self.get_streamdata()
@@ -26,13 +67,18 @@ class StreamFieldable:
 
         for tag in soup.find_all(recursive=False):
             # item = tag
+            module = __name__
+            method = self.tags_to_blocks.get(tag.name, "build_paragraph_block")
+            block_builder = import_string(f"{module}.{method}")
+            print(block_builder)
 
             if not tag.text:
                 continue
 
             # If streamdata is empty add the first tag
             if not streamdata:
-                streamdata.append(self.build_paragaraph_block(tag))
+                # block_name = import_string(f"{module}.{method}")
+                streamdata.append(block_builder(tag))
                 continue
 
             # If the last item in streamdata is a paragraph and this item is a paragraph
@@ -43,13 +89,7 @@ class StreamFieldable:
                 continue
 
             # anything not a paragraph will start a new block
-            streamdata.append(self.build_paragaraph_block(tag))
+            # streamdata.append(self.build_paragraph_block(tag))
+            streamdata.append(block_builder(tag))
 
         return json.dumps(streamdata)
-
-    def build_paragaraph_block(self, tag):
-        block = {
-            "type": "paragraph",
-            "value": f"{tag}",
-        }
-        return block
