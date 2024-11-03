b135e7f4da630249c4d1356c95ab7a3be8ecb602
nickmoreton <nickmoreton@me.com>
2024-11-03 13:21:26 +0000

    Refactor exporter

--- wp_connector/exporter.py
+++ wp_connector/exporter.py
@@ -6,8 +6,6 @@ from taggit.models import Tag
 from blog.models import Author, BlogCategory, BlogPageCategory
 from wp_connector.streamfieldable import StreamFieldable
 
-# TODO: Better logging of messages
-
 
 @dataclass
 class Exporter:
@@ -68,11 +66,22 @@ class Exporter:
             self.obj.WAGTAIL_PAGE_MODEL.split(".")[1],
         )
         self.wagtail_page_model_parent = self.obj.WAGTAIL_PAGE_MODEL_PARENT
+        self.field_mapping = self.obj.FIELD_MAPPING
+        self.stream_field_mapping = self.obj.get_streamfield_mapping()
 
-        # Wagtail always requires a title field value and that fields should
+        if hasattr(self.obj, "author"):
+            self.wagtail_page_model_has_author = True
+
+        if hasattr(self.obj, "tags"):
+            self.wagtail_page_model_has_tags = True
+
+        if hasattr(self.obj, "categories"):
+            self.wagtail_page_model_has_categories = True
+
+        # Wagtail always requires a title field value and that field should
         # always be included in the field_mapping for a page type
 
-        if "title" not in self.obj.FIELD_MAPPING:
+        if "title" not in self.field_mapping:
             self.post_init_messages = {
                 "message": "Title field is required in a FIELD_MAPPING on the wordpress model",
                 "level": "ERROR",
@@ -87,29 +96,80 @@ class Exporter:
             }
             return
 
-        self.field_mapping = self.obj.FIELD_MAPPING
-        self.stream_field_mapping = self.obj.get_streamfield_mapping()
-
-        if hasattr(self.obj, "author"):
-            self.wagtail_page_model_has_author = True
-
-        if hasattr(self.obj, "tags"):
-            self.wagtail_page_model_has_tags = True
-
-        if hasattr(self.obj, "categories"):
-            self.wagtail_page_model_has_categories = True
-
-    def do_create_wagtail_page(self):
-        # The wagtail parent page
-        parent_page = apps.get_model(
+    @property
+    def parent_page(self):
+        return apps.get_model(
             self.wagtail_page_model_parent.split(".")[0],
             self.wagtail_page_model_parent.split(".")[1],
         ).objects.first()
-        if not parent_page:
-            return {"message": "No parent page found.", "level": "ERROR"}
 
+    def get_wordpress_instance(self):
+        return self.admin.model.objects.get(wp_id=self.obj.wp_id)
+
+    def set_fields(self, wagtail_page):
+        for wp_field, wagtail_field in self.field_mapping.items():
+            if self.stream_field_mapping and wp_field in self.stream_field_mapping:
+                stream_field = self.stream_field_mapping[wp_field]
+                streamdata = StreamFieldable(
+                    obj=self.obj,  # for error messages
+                    content=getattr(self.obj, wp_field),
+                )
+                setattr(wagtail_page, stream_field, streamdata.streamdata)
+            else:
+                setattr(wagtail_page, wagtail_field, getattr(self.obj, wp_field))
+
+    def set_author(self, wagtail_page):
+        if self.wagtail_page_model_has_author:
+            # some don't have an author
+            if obj_author := self.obj.author:
+                # is the author already a snippet?
+                author_snippet, created = Author.objects.get_or_create(
+                    name=obj_author.name,
+                )
+                wagtail_page.author = author_snippet
+
+    def set_tags(self, wagtail_page, clear=False):
+        if self.wagtail_page_model_has_tags:
+            # some don't have tags
+            if obj_tags := self.obj.tags.all():
+                if clear:
+                    wagtail_page.tags.clear()
+                for obj_tag in obj_tags:
+                    # is the tag already available in the Tag model?
+                    if tag := Tag.objects.filter(name=obj_tag.name).first():
+                        wagtail_page.tags.add(tag)
+                    else:
+                        tag = Tag.objects.create(name=obj_tag.name)
+                        wagtail_page.tags.add(tag)
+
+    def set_categories(self, wagtail_page, clear=False):
+        if self.wagtail_page_model_has_categories:
+            # some don't have categories
+            if obj_categories := self.obj.categories.all():
+                # remove all previous linked categories
+                if clear:
+                    BlogPageCategory.objects.filter(page=wagtail_page).delete()
+                for obj_category in obj_categories:
+                    # is the category already available in the Category model?
+                    if category := BlogCategory.objects.filter(
+                        name=obj_category.name, slug=obj_category.slug
+                    ).first():
+                        BlogPageCategory.objects.create(
+                            page=wagtail_page,
+                            category=category,
+                        )
+                    else:
+                        category = BlogCategory.objects.create(
+                            name=obj_category.name, slug=obj_category.slug
+                        )
+                        BlogPageCategory.objects.create(
+                            page=wagtail_page,
+                            category=category,
+                        )
+
+    def do_create_wagtail_page(self):
         # The worpress model instance
-        wp_instance = self.admin.model.objects.get(wp_id=self.obj.wp_id)
+        wp_instance = self.get_wordpress_instance()
         # Ignore if the wagtail page is already created
         if wp_instance.wagtail_page_id:
             return {
@@ -118,199 +178,71 @@ class Exporter:
             }
 
         # Create the wagtail page
-        created_wagtail_page = self.wagtail_page_model()
+        wagtail_page = self.wagtail_page_model()
+
         # Set all the fields
-        for wp_field, wagtail_field in self.field_mapping.items():
-            if self.stream_field_mapping and wp_field in self.stream_field_mapping:
-                stream_field = self.stream_field_mapping[wp_field]
-                streamdata = StreamFieldable(
-                    obj=self.obj,  # for error messages
-                    content=getattr(
-                        self.obj,
-                        wp_field,
-                    ),
-                )
-                setattr(
-                    created_wagtail_page,
-                    stream_field,
-                    streamdata.streamdata,
-                )
-            else:
-                setattr(
-                    created_wagtail_page,
-                    wagtail_field,
-                    getattr(
-                        self.obj,
-                        wp_field,
-                    ),
-                )
+        self.set_fields(wagtail_page)
 
         # Set the AUTHOR if the page model has an author
         # Do this before the page is published
-        if self.wagtail_page_model_has_author:
-            # some don't have an author
-            if obj_author := self.obj.author:
-                # is the author already a snippet?
-                author_snippet, created = Author.objects.get_or_create(
-                    name=obj_author.name,
-                )
-                created_wagtail_page.author = author_snippet
+        self.set_author(wagtail_page)
 
         # Set the TAGS if the page model has tags
         # Do this before the page is published
-        if self.wagtail_page_model_has_tags:
-            # some don't have tags
-            if obj_tags := self.obj.tags.all():
-                for obj_tag in obj_tags:
-                    # is the tag already available in the Tag model?
-                    if tag := Tag.objects.filter(name=obj_tag.name).first():
-                        created_wagtail_page.tags.add(tag)
-                    else:
-                        tag = Tag.objects.create(name=obj_tag.name)
-                        created_wagtail_page.tags.add(tag)
+        self.set_tags(wagtail_page)
 
         # Add/Save the page
-        parent_page.add_child(instance=created_wagtail_page)
-        revision = created_wagtail_page.save_revision()
+        parent_page = self.parent_page
+        parent_page.add_child(instance=wagtail_page)
+        revision = wagtail_page.save_revision()
         revision.publish()
 
         # Set the CATEGORIES if the page model has categories
         # Do this after the page is published becuase the relation
         # between Blog post and Category requires a page id
-        if self.wagtail_page_model_has_categories:
-            # some don't have categories
-            if obj_categories := self.obj.categories.all():
-                for obj_category in obj_categories:
-                    # is the category already available in the Category model?
-                    if category := BlogCategory.objects.filter(
-                        name=obj_category.name, slug=obj_category.slug
-                    ).first():
-                        BlogPageCategory.objects.create(
-                            page=created_wagtail_page,
-                            category=category,
-                        )
-                    else:
-                        category = BlogCategory.objects.create(
-                            name=obj_category.name, slug=obj_category.slug
-                        )
-                        BlogPageCategory.objects.create(
-                            page=created_wagtail_page,
-                            category=category,
-                        )
+        self.set_categories(wagtail_page)
 
         # Save the wagtail page ID to the wordpress model
         # so it can be matched later if required
-        wp_instance.wagtail_page_id = created_wagtail_page.id
+        wp_instance.wagtail_page_id = wagtail_page.id
         wp_instance.save()
 
         return {
-            "message": f"Created wagtail page ID:{created_wagtail_page.id}",
+            "message": f"Created wagtail page ID:{wagtail_page.id}",
             "level": "SUCCESS",
         }
 
     def do_update_wagtail_page(self):
-        # The wagtail parent page
-        parent_page = apps.get_model(
-            self.wagtail_page_model_parent.split(".")[0],
-            self.wagtail_page_model_parent.split(".")[1],
-        ).objects.first()
-        if not parent_page:
-            return {"message": "No parent page found.", "level": "ERROR"}
-
         # The worpress model instance
-        wp_instance = self.admin.model.objects.get(wp_id=self.obj.wp_id)
+        wp_instance = self.get_wordpress_instance()
         if not wp_instance.wagtail_page_id:
             return f"Wagtail page not created. {wp_instance.wagtail_page_id}"
 
         # Update the wagtail page
-        updated_wagtail_page = self.wagtail_page_model.objects.get(
+        wagtail_page = self.wagtail_page_model.objects.get(
             id=wp_instance.wagtail_page_id,
         )
         # Set all the fields
-        for wp_field, wagtail_field in self.field_mapping.items():
-            if self.stream_field_mapping and wp_field in self.stream_field_mapping:
-                stream_field = self.stream_field_mapping[wp_field]
-                streamdata = StreamFieldable(
-                    obj=self.obj,  # for error messages
-                    content=getattr(
-                        self.obj,
-                        wp_field,
-                    ),
-                )
-                setattr(
-                    updated_wagtail_page,
-                    stream_field,
-                    streamdata.streamdata,
-                )
-            else:
-                setattr(
-                    updated_wagtail_page,
-                    wagtail_field,
-                    getattr(
-                        self.obj,
-                        wp_field,
-                    ),
-                )
+        self.set_fields(wagtail_page)
 
         # Set the AUTHOR if the page model has an author
         # Do this before the page is published
-        if self.wagtail_page_model_has_author:
-            # some don't have an author
-            if obj_author := self.obj.author:
-                # is the author already a snippet?
-                author_snippet, created = Author.objects.get_or_create(
-                    name=obj_author.name,
-                )
-                updated_wagtail_page.author = author_snippet
+        self.set_author(wagtail_page)
 
         # Set the TAGS if the page model has tags
         # Do this before the page is published
-        if self.wagtail_page_model_has_tags:
-            # some don't have tags
-            if obj_tags := self.obj.tags.all():
-                updated_wagtail_page.tags.clear()
-                for obj_tag in obj_tags:
-                    # is the tag already available in the Tag model?
-                    if tag := Tag.objects.filter(name=obj_tag.name).first():
-                        updated_wagtail_page.tags.add(tag)
-                    else:
-                        tag = Tag.objects.create(name=obj_tag.name)
-                        updated_wagtail_page.tags.add(tag)
+        self.set_tags(wagtail_page, clear=True)
 
         # Set the CATEGORIES if the page model has categories
         # Do this after the page is published becuase the relation
         # between Blog post and Category requires a page id
-        if self.wagtail_page_model_has_categories:
-            # some don't have categories
-            if obj_categories := self.obj.categories.all():
-                # remove all previous linked categories
-                BlogPageCategory.objects.filter(page=updated_wagtail_page).delete()
-                for obj_category in obj_categories:
-                    # is the category already available in the Category model?
-                    if category := BlogCategory.objects.filter(
-                        name=obj_category.name, slug=obj_category.slug
-                    ).first():
-                        BlogPageCategory.objects.create(
-                            page=updated_wagtail_page,
-                            category=category,
-                        )
-                    else:
-                        category = BlogCategory.objects.create(
-                            name=obj_category.name, slug=obj_category.slug
-                        )
-                        BlogPageCategory.objects.create(
-                            page=updated_wagtail_page,
-                            category=category,
-                        )
+        self.set_categories(wagtail_page, clear=True)
 
         # Update/Save the page
-        revision = updated_wagtail_page.save_revision()
+        revision = wagtail_page.save_revision()
         revision.publish()
 
-        # Dont' update the wagtail page ID here
-        # once they are reated they should not change
-
         return {
-            "message": f"Updated wagtail page ID:{updated_wagtail_page.id}",
+            "message": f"Updated wagtail page ID:{wagtail_page.id}",
             "level": "SUCCESS",
         }
