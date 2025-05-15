b9256f3d1a6fca157d175bca7825517586a97f04
Nick Moreton <nickmoreton@me.com>
2025-05-15 21:50:05 +0100

    Fix paths

--- wordpress.docker/init.sh
+++ wordpress.docker/init.sh
@@ -7,8 +7,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 
 # downlaod the Wordpress theme sample XML file
-mkdir -p /xml
-curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml -o /xml/import.xml
+mkdir -p xml
+curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml -o xml/import.xml
 
 # Install and setup Wordpress
 php wp-cli.phar core install --allow-root --url=localhost:8888 --title=WordPress --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
@@ -17,7 +17,7 @@ php wp-cli.phar core install --allow-root --url=localhost:8888 --title=WordPress
 php wp-cli.phar plugin install wordpress-importer --activate --allow-root
 
 # Import the Wordpress XML file
-php wp-cli.phar import /xml/import.xml --authors=create --allow-root
+php wp-cli.phar import xml/import.xml --authors=create --allow-root
 
 # Enable the json API
 php wp-cli.phar rewrite structure /%postname%/ --allow-root
