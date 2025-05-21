e78ce2be6fbfc426f0d7902846458f5b6e813ca8
Nick Moreton <nickmoreton@me.com>
2025-05-21 16:30:25 +0100

    Remove the static folder

--- app/static/js/app.js
+++
@@ -1,2 +0,0 @@
-(()=>{var e=class{constructor(s=3){this.message="Hello from Wagtail starter kit!",this.times=s}showMessage(){for(let s=0;s<this.times;s++)console.log(this.message)}},t=new e;t.showMessage();})();
-//# sourceMappingURL=app.js.map
